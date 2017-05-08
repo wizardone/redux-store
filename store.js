160178c55497108a9a21f385078dcbb801d55083
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-05-08 19:26:19 +0300

    add store

---
+++ store.js
@@ -0,0 +1,14 @@
+const store = () => {
+
+  let initialState = undefined;
+
+  let state = () => {}
+  let getState = () => {}
+  let dispatch(action) => {}
+
+  return {
+    state,
+    getState,
+    dispatch
+  }
+}
