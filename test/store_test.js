49b140adf2514653cb3bb13b4a5b4bdba69c7be8
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-05-11 19:28:44 +0300

    spec the current store functionality, almost there

--- test/store_test.js
+++ test/store_test.js
@@ -8,28 +8,34 @@ let reducer = (state = 0, action) => {
     return state
   }
 }
-
 let expect = require('expect.js');
-let store = require('../store.js')(reducer);
 
 describe('store', () => {
   it('has an undefined initialState', () => {
+    const store = require('../store.js')(reducer);
+
     expect(store.initialState).to.be(undefined)
   });
 });
 
 describe('getState', () => {
   it('returns the current state of the store', () => {
+    const store = require('../store.js')(reducer);
+
     expect(store.getState()).to.eql(0);
   });
 });
 
 describe('subscribe', () => {
   it('it throws an error if no function is supplied', () => {
+    const store = require('../store.js')(reducer);
+
     expect(() => store.subscribe('sstring')).to.throw
   });
 
   it('subscribes to the listeners array', () => {
+    const store = require('../store.js')(reducer);
+
     expect(store.subscribe(() => console.log('subscribed'))).to.eql(true)
   });
 });
@@ -40,12 +46,14 @@ describe('dispatch', () => {
   });
 
   it('dispatches an action to the store', () => {
+    const store = require('../store.js')(reducer);
     let action = {type: 'INCREMENT'};
 
     expect(store.dispatch(action)).to.eql(action);
   });
 
   it('updates the state of the store', () => {
+    const store = require('../store.js')(reducer);
     let action = {type: 'INCREMENT'};
 
     expect(store.getState()).to.eql(0);
